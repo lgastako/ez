9b9286e4904d3b9515fbfd8a71e232219335c0d9
John Evans <lgastako@gmail.com>
2009-04-29 23:51:34 -0700

    Initial paster template done.

--- ez/templates.py
+++ ez/templates.py
@@ -6,5 +6,5 @@ class EzProjectTemplate(Template):
     _template_dir = "project_templates/default_ez_project"
     summary = "An ez web framework project"
     vars = [
-        var("gae_app_name", "my_app"),
+        var("gae_app_name", "Google AppEngine app name (all lowercase, no spaces)"),
     ]
